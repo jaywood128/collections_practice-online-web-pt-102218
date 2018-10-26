5f30170f325eb0520a923a4d9c9bc64a8476f682
Johnathon Wood <jaywood128@gmail.com>
2018-10-26 17:09:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,5 +6,7 @@ def sort_array_desc(array)
 end
 def sort_array_char_count(array)
   array.sort {|x, y| x.length <=> y.length }
-
+end
+def swap_elements(array)
+  array.sort {|array[x], index[y]| array[x] <=> array[y]}
 end
