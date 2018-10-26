9fbb1113963645d073e9021e0a6e273a3f9edb35
Johnathon Wood <jaywood128@gmail.com>
2018-10-26 17:41:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,7 +8,7 @@ def sort_array_char_count(array)
   array.sort {|x, y| x.length <=> y.length }
 end
 def swap_elements(array)
-  array[0], array[1], array[2] = array[0],array[2], array[1]
+  array[0], array[1], array[2] = array[0], array[2], array[1]
 end
 def reverse_array(array)
   array.reverse
